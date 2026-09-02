0296f1394ff65b8e69a6b1482821da6fc6fb19ae
Bricks2Clicks <sue@bricks2clicks.online>
2026-09-01 21:28:44 -0600

    Updates homepage text and mobile leaf icon

--- public/js/icons.js
+++ public/js/icons.js
@@ -1,10 +1,3 @@
-export const MAPLE_LEAF_SRC = '/icons/maple-leaf.png';
-
-const RED = '#d80621';
-
 export function logoMarkSvg() {
-  return `<svg class="logo-mark" viewBox="0 0 32 32" aria-hidden="true">
-    <rect width="32" height="32" rx="6" fill="${RED}"/>
-    <image class="logo-maple" href="${MAPLE_LEAF_SRC}" x="6" y="5" width="20" height="22" preserveAspectRatio="xMidYMid meet"/>
-  </svg>`;
+  return `<span class="logo-mark" aria-hidden="true"><span class="logo-maple"></span></span>`;
 }
