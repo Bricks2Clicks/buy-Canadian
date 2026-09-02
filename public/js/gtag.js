00a98373d6b9afebbabb5cdecf125f32426ac644
Bricks2Clicks <sue@bricks2clicks.online>
2026-09-01 22:34:50 -0600

    update to gtag so repo forks can't reuse my GA tag

--- public/js/gtag.js
+++ public/js/gtag.js
@@ -1,11 +1,17 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-MBNEYVYW3J');
+(function () {
+  const ALLOWED_HOSTS = ['buycanadian.bricks2clicks.online'];
+  if (!ALLOWED_HOSTS.includes(location.hostname)) return;
 
-const script = document.createElement('script');
-script.async = true;
-script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MBNEYVYW3J';
-document.head.prepend(script);
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-MBNEYVYW3J');
+
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MBNEYVYW3J';
+  document.head.prepend(script);
+})();
