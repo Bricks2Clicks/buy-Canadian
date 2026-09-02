e9a26007d51669922fbf0dd10fbc9c7b9cd08aff
Bricks2Clicks <sue@bricks2clicks.online>
2026-09-01 22:28:54 -0600

    Adds Google Analytics

---
+++ public/js/gtag.js
@@ -0,0 +1,11 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-MBNEYVYW3J');
+
+const script = document.createElement('script');
+script.async = true;
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MBNEYVYW3J';
+document.head.prepend(script);
